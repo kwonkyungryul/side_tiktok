7afa9804fe3508f260c65c8b2b44085672882862
kwonkyungryul <kkr0787@naver.com>
2023-08-28 22:29:03 +0900

    Initialized

---
+++ src/main/java/com/example/side_tiktok/SideTiktokApplication.java
@@ -0,0 +1,13 @@
+package com.example.side_tiktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideTiktokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideTiktokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/side_tiktok/SideTiktokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.side_tiktok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideTiktokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
